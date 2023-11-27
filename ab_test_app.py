3270a236d90a108df7a1c8fe360027603e749e4d
Andrea DC <116172392+andreadcbrainly@users.noreply.github.com>
2023-11-27 22:35:21 +0100

    Update ab_test_app.py

--- ab_test_app.py
+++ ab_test_app.py
@@ -18,8 +18,8 @@ def load_data(file):
 # Function to display A/B test results
 def display_results(group_a, group_b, metric, p_value):
     st.write("### A/B Test Results:")
-    st.write(f"- **Group A ({group_a['name'].unique()[0]}):** Mean {metric}: {group_a[metric].mean()}")
-    st.write(f"- **Group B ({group_b['name'].unique()[0]}):** Mean {metric}: {group_b[metric].mean()}")
+    st.write(f"- **Group A ({group_a_name}):** Mean {metric}: {group_a[metric].mean()}")
+    st.write(f"- **Group B ({group_b_name}):** Mean {metric}: {group_b[metric].mean()}")
     st.write(f"- **p-value:** {p_value}")
 
 # Streamlit app
@@ -37,8 +37,9 @@ def main():
 
         # Select test groups and metrics
         st.write("### Configure A/B Test")
-        test_group_a = st.selectbox("Select Group A", data['group'].unique())
-        test_group_b = st.selectbox("Select Group B", data['group'].unique())
+        test_groups = data['group'].unique()
+        test_group_a = st.selectbox("Select Group A", test_groups)
+        test_group_b = st.selectbox("Select Group B", test_groups)
         metric_to_evaluate = st.selectbox("Select Metric", data.columns)
 
         group_a_data = data[data['group'] == test_group_a]
@@ -47,6 +48,8 @@ def main():
         # Perform t-test and display results
         if st.button("Run A/B Test"):
             t_statistic, p_value = perform_t_test(group_a_data, group_b_data, metric_to_evaluate)
+            group_a_name = group_a_data['name'].unique()[0] if 'name' in group_a_data.columns else test_group_a
+            group_b_name = group_b_data['name'].unique()[0] if 'name' in group_b_data.columns else test_group_b
             display_results(group_a_data, group_b_data, metric_to_evaluate, p_value)
 
 if __name__ == "__main__":
